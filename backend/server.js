082a16b2ad3090faecd7049a6de9a88b4b0b9f51
Joseph <jotreewater@gmail.com>
2024-10-28 15:35:11 -0700

    Set up express, packages, and dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
-console.log('server');
+// env setup
+const dotenv = require('dotenv').config();
+//console.log(`Server is in mode: ${process.env.NODE_ENV}`);
+//console.log(`Server will run on port: ${process.env.PORT}`);
+
+// express setup
+const express = require('express');
+const app = express();
+app.use(express.json()); // to allow requests with JSON payloads
+app.use(express.urlencoded({ extended: false })); // Uses querystring node library to parse form data
+
+// routes
+
+// middleware
+
+// default response
+app.get('/', (req, res) => {
+  res.status(200).json({ message: `You've hit the jotrier API` });
+});
+
+// entrypoint
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}`);
+});
